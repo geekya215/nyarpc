3c26dff2e931f945fb3e779eff2d3f03f0a5586f
Lin Yang <geekya215@gmail.com>
2024-05-02 18:53:50 +0800

    add ProtocolFrameDecoder

--- src/main/java/io/geekya215/nyarpc/protocal/ProtocolFrameDecoder.java
+++ src/main/java/io/geekya215/nyarpc/protocal/ProtocolFrameDecoder.java
@@ -4,9 +4,10 @@ import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 
 public final class ProtocolFrameDecoder extends LengthFieldBasedFrameDecoder {
     public static final int DEFAULT_MAX_FRAME_LENGTH = 65535;
+    public static final int DEFAULT_LENGTH_FIELD_LENGTH = 4;
 
     public ProtocolFrameDecoder() {
-        this(DEFAULT_MAX_FRAME_LENGTH,  Protocol.HEADER_LENGTH,4);
+        this(DEFAULT_MAX_FRAME_LENGTH, Protocol.HEADER_LENGTH, DEFAULT_LENGTH_FIELD_LENGTH);
     }
 
     public ProtocolFrameDecoder(int maxFrameLength, int lengthFieldOffset, int lengthFieldLength) {
