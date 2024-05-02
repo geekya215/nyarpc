fe165bdce1df9daffa8ed5b846da3d31d2b26097
Lin Yang <geekya215@gmail.com>
2024-05-02 18:51:27 +0800

    add ClassUtil

---
+++ src/main/java/io/geekya215/nyarpc/util/ClassUtil.java
@@ -0,0 +1,55 @@
+package io.geekya215.nyarpc.util;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+public final class ClassUtil {
+    private ClassUtil() {
+    }
+
+    public static @NotNull List<Class<?>> scanClassesWithAnnotation(
+            @NotNull String packageName,
+            @NotNull Class<? extends Annotation> annotationClass)
+            throws IOException, ClassNotFoundException
+    {
+        final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        final String path = packageName.replace('.', '/');
+        final Enumeration<URL> resources = classLoader.getResources(path);
+
+        final List<Class<?>> classes = new ArrayList<>();
+
+        while (resources.hasMoreElements()) {
+            final URL resource = resources.nextElement();
+
+            if (resource.getProtocol().equals("file")) {
+                final File directory = new File(resource.getFile());
+
+                if (directory.exists()) {
+                    final File[] files = directory.listFiles();
+
+                    if (files != null) {
+                        for (File file : files) {
+                            if (file.isDirectory()) {
+                                classes.addAll(scanClassesWithAnnotation(packageName + "." + file.getName(), annotationClass));
+                            } else if (file.getName().endsWith(".class")) {
+                                final String className = packageName + '.' + file.getName().substring(0, file.getName().length() - 6);
+                                Class<?> clazz = Class.forName(className);
+                                if (clazz.isAnnotationPresent(annotationClass)) {
+                                    classes.add(clazz);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return classes;
+    }
+}
